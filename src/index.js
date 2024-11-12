9b6794dd01e927c905864355aab10fe4e98123e2
Niteen Kumar <niteen22336@iiitd.ac.in>
2024-11-12 22:12:19 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import app from './app.js' ;
+
+
+const port = process.env.PORT || 5000 ;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!') ;
+})
+
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`) ;
+})
